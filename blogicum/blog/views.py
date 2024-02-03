d37282da8cc81c3f37091c41691ace43ea9a7e54
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2024-02-03 22:17:08 +0300

    Add and update views

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView
+from .models import Post, Category, User
 import datetime
 
 
-POSTS_ON_HOMEPAGE = 5
+POSTS_ON_PAGE = 10
 
 
 class PostMixin:
@@ -19,7 +21,7 @@ class PostMixin:
 
 class PostListView(PostMixin, ListView):
     template_name = 'blog/index.html'
-    paginate_by = POSTS_ON_HOMEPAGE
+    paginate_by = POSTS_ON_PAGE
 
 
 class PostDetailView(PostMixin, DetailView):
@@ -27,21 +29,51 @@ class PostDetailView(PostMixin, DetailView):
     pk_url_kwarg = 'id'
 
 
-class CategoryDetailView(DetailView):
-    model = Category
+class CategoryPostListView(ListView):
     template_name = 'blog/category.html'
-    slug_url_kwarg = 'category_slug'
-    queryset = Category.objects.filter(
-        is_published=True,
-    )
+    paginate_by = POSTS_ON_PAGE
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['post_list'] = Post.objects.select_related(
+    def get_queryset(self):
+        return Post.objects.select_related(
             'author', 'location', 'category'
         ).filter(
             pub_date__lte=datetime.datetime.now(),
             is_published=True,
-            category__slug=self.kwargs.get('category_slug'),
+            category__slug=self.kwargs.get('category_slug')
         )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = get_object_or_404(Category.objects.filter(
+            is_published=True,
+            slug=self.kwargs.get('category_slug'),
+        ))
         return context
+
+
+class UserPostListView(ListView):
+    template_name = 'blog/profile.html'
+    paginate_by = POSTS_ON_PAGE
+
+    def get_queryset(self):
+        return Post.objects.select_related(
+            'author', 'location', 'category'
+        ).filter(
+            author__username=self.kwargs.get('username')
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = get_object_or_404(User.objects.filter(
+            username=self.kwargs.get('username')
+        ))
+        return context
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'blog/user.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    fields = ('first_name', 'last_name', 'username', 'email',)
+    success_url = reverse_lazy('blog:index')
