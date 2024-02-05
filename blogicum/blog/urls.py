8b03072df58b8206640febdb94db5b5c41521b4e
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2024-02-06 00:07:33 +0300

    Add paths for comments

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -30,4 +30,13 @@ urlpatterns = [
         views.UserUpdateView.as_view(),
         name='edit_profile',
     ),
+    path(
+        'posts/<int:post_id>/comment/', views.CommentCreateView.as_view(), name='add_comment'
+    ),
+    path(
+        'posts/<int:post_id>/edit_comment/<int:comment_id>/', views.CommentUpdateView.as_view(), name='edit_comment'
+    ),
+    path(
+        'posts/<int:post_id>/delete_comment/<int:comment_id>/', views.CommentDeleteView.as_view(), name='delete_comment'
+    ),
 ]
