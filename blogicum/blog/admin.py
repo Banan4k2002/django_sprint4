6ee4bae66e8de2555ac293a3305bd02a63d16de1
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2024-02-18 18:48:29 +0300

    Add fieldsets and image_icon in PostAdmin

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from blog.models import Category, Comment, Location, Post
 
@@ -42,6 +43,7 @@ class PostAdmin(admin.ModelAdmin):
         'category',
         'is_published',
         'created_at',
+        'image_icon',
     )
     list_editable = (
         'author',
@@ -57,6 +59,38 @@ class PostAdmin(admin.ModelAdmin):
         'location',
         'category',
     )
+    fieldsets = (
+        (
+            'Содержание публикации',
+            {
+                'fields': (
+                    'title',
+                    'text',
+                )
+            },
+        ),
+        (
+            'Подробная информация',
+            {
+                'fields': (
+                    'is_published',
+                    'author',
+                    'location',
+                    'category',
+                    'pub_date',
+                    'image',
+                )
+            },
+        ),
+    )
+
+    def image_icon(self, object):
+        if object.image:
+            return format_html(
+                '<img src="{}" width="100" height="100">', object.image.url
+            )
+
+    image_icon.short_description = 'Изображение'
 
 
 @admin.register(Comment)
