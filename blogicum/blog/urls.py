f1d74a4c4ff1f3c9a73ba86dbbbd68d9470d3981
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2024-02-19 08:41:04 +0300

    Split urlpatterns and update edit_profile url

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,55 +1,63 @@
-from django.urls import path
+from django.urls import include, path
 
 from blog import views
 
 app_name = 'blog'
 
-urlpatterns = [
+posts_urls = [
     path(
-        'posts/<int:post_id>/',
+        '<int:post_id>/',
         views.PostDetailView.as_view(),
         name='post_detail',
     ),
     path(
-        'posts/<int:post_id>/edit/',
+        '<int:post_id>/edit/',
         views.PostUpdateView.as_view(),
         name='edit_post',
     ),
     path(
-        'posts/<int:post_id>/delete/',
+        '<int:post_id>/delete/',
         views.PostDeleteView.as_view(),
         name='delete_post',
     ),
-    path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
+    path('create/', views.PostCreateView.as_view(), name='create_post'),
     path(
-        'category/<slug:category_slug>/',
-        views.CategoryPostListView.as_view(),
-        name='category_posts',
-    ),
-    path(
-        'profile/<slug:username>/',
-        views.UserPostListView.as_view(),
-        name='profile',
-    ),
-    path(
-        'profile/<slug:username>/edit/',
-        views.UserUpdateView.as_view(),
-        name='edit_profile',
-    ),
-    path(
-        'posts/<int:post_id>/comment/',
+        '<int:post_id>/comment/',
         views.CommentCreateView.as_view(),
         name='add_comment',
     ),
     path(
-        'posts/<int:post_id>/edit_comment/<int:comment_id>/',
+        '<int:post_id>/edit_comment/<int:comment_id>/',
         views.CommentUpdateView.as_view(),
         name='edit_comment',
     ),
     path(
-        'posts/<int:post_id>/delete_comment/<int:comment_id>/',
+        '<int:post_id>/delete_comment/<int:comment_id>/',
         views.CommentDeleteView.as_view(),
         name='delete_comment',
+    ),
+]
+
+profile_urls = [
+    path(
+        'edit/',
+        views.UserUpdateView.as_view(),
+        name='edit_profile',
+    ),
+    path(
+        '<slug:username>/',
+        views.UserPostListView.as_view(),
+        name='profile',
+    ),
+]
+
+urlpatterns = [
+    path('posts/', include(posts_urls)),
+    path("profile/", include(profile_urls)),
+    path(
+        'category/<slug:category_slug>/',
+        views.CategoryPostListView.as_view(),
+        name='category_posts',
     ),
     path('', views.PostListView.as_view(), name='index'),
 ]
